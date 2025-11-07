f435b174364493588eeda5e05725a403fda1d3f7
pablojimora <pablojimora@gmail.com>
2025-11-07 10:39:52 -0500

    fix services

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL_API = '/api/';
+const URL_API = process.env.NEXT_PUBLIC_API_URL;
 
 
 export const createUser = async (data: {
@@ -10,7 +10,7 @@ export const createUser = async (data: {
   password: string;
 }) => {
   try {
-    const response = await axios.post("/api/register", data, {
+    const response = await axios.post(`${URL_API}/api/auth/register`, data, {
       headers: { "Content-Type": "application/json" },
     });
     return response.data;
@@ -29,7 +29,7 @@ export const loginUser = async (data: {
   password: string;
 }) => {
   try {
-    const response = await axios.post(`${URL_API}login`, data, {
+    const response = await axios.post(`${URL_API}/api/auth/login`, data, {
       headers: { "Content-Type": "application/json" },
     });
     return response.data;
